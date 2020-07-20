345b371fdac5ad1cadb9ca22bec160bfba12d33c
shaikha121 <shaikha121@users.noreply.github.com>
2020-07-20 19:06:15 +0300

    added class
    help keep track of money

--- project_alpha.py
+++ project_alpha.py
@@ -23,7 +23,7 @@ if answer.lower().strip() == "yes":
 
 else:
     print()
-    print("No...wait...please I promise it will be fun ")
+    print("No...wait...please I promise it will be fun. ")
     exit()
 
 crew = input("please insert 3 names seperated by a comma: ")
@@ -40,7 +40,7 @@ print("You and your crew have decided to rob the LCL bank in France. Attempting 
 print("You finally arrive to your destination.\n")
 random_crew = random.choice(user_crew)
 
-print("You appoint, "+str(random_crew)+" to keep watch in case any of the hostages attempt to escape or call for help \n")
+print("You appoint, "+str(random_crew)+" to keep watch in case any of the hostages attempt to escape or call for help. \n")
 
 print("After a while one of your members, "+str(random_crew)+" ,informs you that the 2,000,000 euros have been retrevied.\n")
 
@@ -56,21 +56,22 @@ if answer.lower().strip() == "sacrifice":
     print("You chose to sacrifice yourself.\n")
     print("You walk out with your hands above your head.\n")
     print("You give yourself up to the police,saving your team in the process. \n")
-    print("You spent the rest of your life rotting away in jail \n")
+    print("You spent the rest of your life rotting away in jail. \n")
     print("GAME OVER")
     exit()
 
 elif answer.lower().strip() == "escape":
     random_crew1 = random.choice(user_crew)
+    print()
     print("You choose to escape.\n")
-    print("While you and your team are attempting to escape \n")
-    print("One of your team members, "+str(random_crew1)+" ,is suddenly shot in the leg, enabling them to continue.\n" )
+    print("While you and your team are attempting to escape,one of your team members, "+str(random_crew1)+" ,is suddenly shot in the leg, preventing them from continuing.\n" )
     print ("You have one option: save them or leave them.\n")
     print("Type save or leave below: \n")
     answer = input("So what will it be? ")
     print()
-# add def so that money can be tracked
+
     if answer.lower().strip() == "save":
+        user_crew.remove(random_crew1)
         print("You go back and attempt to save them.\n")
         print("You manage to help them up to their feet, providing your shoulder for support.\n")
         print("Before you can make it to the back exit you hear a BANG!!! \n")
@@ -82,3 +83,17 @@ elif answer.lower().strip() == "escape":
         print("You look back up finding officers rushing towards you with their guns aiming staright at you. \n")
         print("Having had no time to grief you grab the straped bag containing some of the money from his shoulder and place it on yours.Causing some of the money to spill from the bag. \n")
         print("Having no time to retrieve it do the only thing you can think of, you run, trying to make it back to your remaining crew members waiting for you by the get away car out back. \n")
+
+class Amount():
+    def __init__(self):
+        self.result = 2000000
+
+    def subtract(self,x):
+        self.result =  self.result - int(x)
+
+    def get_result(self):
+        return self.result
+
+A = Amount()
+A.subtract(random.randrange(1000,100000))
+print(A.get_result())
